343526af4582e2b3510100703769fb189e945176
unknown <rizqiramdanigit@gmail.com>
2021-05-29 10:41:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/quiz10118069/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.quiz10118069;
+/**
+ * Tanggal Pengerjaan : Sabtu, 329 Mei 2021
+ * Deskripsi Pekerjaan: Membuat layouting
+ * Nim : 10118069
+ * Nama : Muhamad Rizqi Ramdani
+ * Kelas : IF-2
+ **/
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
